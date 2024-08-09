b1c5ed8b686c9376fa634341a27f07a092842e53
Franz Jimeno <franz.jimeno@gmail.com>
2024-08-09 11:09:55 +0200

    Commit inicial 2

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -19,3 +19,39 @@ exports.createMember = async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 };
+
+exports.getMember = async (req, res) => {
+  try {
+    const member = await Member.findById(req.params.id);
+    if (!member) {
+      return res.status(404).json({ message: 'Member not found' });
+    }
+    res.json(member);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.updateMember = async (req, res) => {
+  try {
+    const member = await Member.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!member) {
+      return res.status(404).json({ message: 'Member not found' });
+    }
+    res.json(member);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.deleteMember = async (req, res) => {
+  try {
+    const member = await Member.findByIdAndDelete(req.params.id);
+    if (!member) {
+      return res.status(404).json({ message: 'Member not found' });
+    }
+    res.json({ message: 'Member deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
