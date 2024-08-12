87eae986f2f2dcac571ff318b11535857b0a7fcd
Franz Jimeno Demuth <franz.jimeno+imac@gmail.com>
2024-08-12 08:09:20 +0200

    Nuevo programa con Vite

---
+++ huca_gestion_grupos/src/components/layout.jsx
@@ -0,0 +1,7 @@
+export const Layout = (props) => {
+  return (
+    <section className="w-5/6 mx-auto px-8 pt-10">
+      {props.children}
+    </section>
+  )
+}
---
+++ huca_gestion_grupos/src/pages/cursos.jsx
@@ -0,0 +1,5 @@
+export const Cursos = () => {
+  return (
+    <div>Cursos</div>
+  )
+}
